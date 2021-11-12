804cd9a326adb75004adf1ff74283c3de7795ba7
Kshitij Kotnis <kkotnis13@gmail.com>
2021-11-12 17:41:10 +0530

    commit 2

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import {PostCard,Categories,PostWidget} from '../components'
 
 const posts = [
   {title:'React Testing',excerpt:'Learn React Testing'},
@@ -9,21 +10,19 @@ export default function Home() {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
-        <title>CMS Blog</title>
+        <title>Blogit</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div>
           {posts.map((post,index)=>(
-            <div className="lg:col-span-8 col-span-1">
-              {post.title}
-              {post.excerpt}
-            </div>
+            <PostCard post={post} key={post.title}/>
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
             <div className="lg:sticky relative top-8">
-
+              <PostWidget/>
+              <Categories/>
             </div>
         </div>
       </div>
