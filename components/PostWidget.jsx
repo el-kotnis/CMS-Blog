502633270cf8122a10bf52cf76d07d44be4433d8
Kshitij Kotnis <kkotnis13@gmail.com>
2021-11-15 23:04:24 +0530

    commit 7

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -9,10 +9,9 @@ const PostWidget = ({categories,slug}) => {
         if(slug){
             getSimilarPosts(categories,slug).then((result)=>setRelatedPosts(result))
         }else{
-            getRecentPosts(slug).then((result)=>setRelatedPosts(result))
+            getRecentPosts().then((result)=>setRelatedPosts(result))
         }
     },[slug])
-    console.log(relatedPosts)
     return (
         <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
             <h3 className="text-xl mb-8 font-semibold border-b pb-4">{slug ? 'Related Posts' : 'Recent Posts'}</h3>
@@ -20,7 +19,6 @@ const PostWidget = ({categories,slug}) => {
                 <div key={index} className="flex items-center w-full mb-4">
                     <div className="w-16 flex-none">
                         <img
-                            loader={grpahCMSImageLoader}
                             alt={post.title}
                             height="60px"
                             width="60px"
--- services/index.js
+++ services/index.js
@@ -41,39 +41,26 @@ export const getPosts = async()=>{
     return result.postsConnection.edges
 }
 
-export const getRecentPosts = async (slug) => {
+export const getRecentPosts = async () => {
   const query = gql`
-      query GetPostDetails($slug : String!) {
-        post(where: {slug: $slug}) {
-          title
-          excerpt
-          featuredImage {
-            url
-          }
-          author{
-            name
-            bio
-            photo {
-              url
-            }
-          }
-          createdAt
-          slug
-          content {
-            raw
-          }
-          categories {
-            name
-            slug
-          }
+    query GetPostDetails() {
+      posts(
+        orderBy: createdAt_ASC
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
         }
+        createdAt
+        slug
       }
-    `;
-  
-  const result = await request(graphqlAPI, query, { slug });
-  
-  return result.post;
-}
+    }
+  `;
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+};
 
 export const getSimilarPosts = async (categories, slug) => {
     const query = gql`
@@ -91,7 +78,7 @@ export const getSimilarPosts = async (categories, slug) => {
         }
       }
     `;
-  const result = await request(graphqlAPI, query, { slug, categories });
+  const result = await request(graphqlAPI, query, { categories, slug });
   
   return result.posts;
 };
@@ -109,4 +96,38 @@ export const getCategories = async () => {
   const result = await request(graphqlAPI, query);
 
   return result.categories;
+};
+
+export const getPostDetails = async (slug) => {
+  const query = gql`
+    query GetPostDetails($slug : String!) {
+      post(where: {slug: $slug}) {
+        title
+        excerpt
+        featuredImage {
+          url
+        }
+        author{
+          name
+          bio
+          photo {
+            url
+          }
+        }
+        createdAt
+        slug
+        content {
+          raw
+        }
+        categories {
+          name
+          slug
+        }
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query, { slug });
+
+  return result.post;
 };
