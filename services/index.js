b77525fe77d967fe0a49f8428e2ddcbeaf10513f
Kshitij Kotnis <kkotnis13@gmail.com>
2021-11-28 17:06:05 +0530

    commit 10

--- services/index.js
+++ services/index.js
@@ -131,3 +131,16 @@ export const getPostDetails = async (slug) => {
 
   return result.post;
 };
+
+//make an http request to my ow next js backend
+export const submitComment = async (obj) => {
+  const result = await fetch('/api/comments', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(obj),
+  });
+
+  return result.json();
+};
