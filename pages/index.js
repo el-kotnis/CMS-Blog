227436c33f951fbfb4cc832dd25294fda256b936
Kshitij Kotnis <kkotnis13@gmail.com>
2021-11-29 23:55:53 +0530

    commit 13

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import {PostCard,Categories,PostWidget} from '../components'
 import {getPosts} from '../services'
-
+import { FeaturedPosts } from '../sections'
 export default function Home({posts}) {
   return (
     <div className="container mx-auto px-10 mb-8">
@@ -9,6 +9,7 @@ export default function Home({posts}) {
         <title>BlogIt</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <FeaturedPosts/>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post)=><PostCard post={post.node} key={post.title}/>)}
