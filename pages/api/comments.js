c164de156313ae008028a1d0dc1dde511547fe96
Kshitij Kotnis <kkotnis13@gmail.com>
2021-11-29 18:05:44 +0530

    commit 11

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -1,9 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { GraphQLClient,gql } from "graphql"
+import { GraphQLClient,gql } from "graphql-request"
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
-
-export default function comments(req, res) {
+const graphcmsToken = process.env.GRAPHCMS_TOKEN;
+export default async function comments(req, res) {
   const graphQLClient = new GraphQLClient(graphqlAPI,{
     headers:{
       authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`
